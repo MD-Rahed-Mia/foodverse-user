f76d070915acc783cf0c8d74cb4d390d4fc65569
Md Rahed Mia <135752931+MD-Rahed-Mia@users.noreply.github.com>
2024-12-08 18:55:30 +0600

    update menu review

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -72,7 +72,9 @@ export default function MenuCard({ detail }) {
                 d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
               />
             </svg>
-            <p className="px-2 font-bold text-gray-700">4.3</p>
+            <p className="px-2 font-bold text-gray-700">
+              {detail?.averageRating || 4}
+            </p>
           </div>
         </div>
         {/* <div>
@@ -123,7 +125,9 @@ export default function MenuCard({ detail }) {
                       d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
                     />
                   </svg>
-                  <span className="px-2 font-bold text-gray-700">4.7</span>
+                  <span className="px-2 font-bold text-gray-700">
+                    {detail?.averageRating}
+                  </span>
                 </div>
               </div>
             </div>
@@ -239,7 +243,7 @@ const AddonItem = ({
     } else {
       // If the item is deselected, remove it from the addonList
       const filterAddon = addonList.filter(
-        (it) => it._id.toString() === item._id.toString()
+        (it) => it._id.toString() === item._id.toString(),
       );
 
       // Update the addonList without the deselected item
--- src/components/RestaurantPage.js
+++ src/components/RestaurantPage.js
@@ -88,8 +88,8 @@ const RestaurantPage = () => {
                   d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
                 />
               </svg>
-              <span className="text-lg font-semibold text-gray-800">4.7</span>
-              <span className="text-sm text-gray-600">5000+ ratings</span>
+              <span className="text-lg font-semibold text-gray-800">{ restaurant?.averageReview}</span>
+              <span className="text-sm text-gray-600">{ restaurant?.totalReviews}+ ratings</span>
             </div>
             <div className="text-sm text-gray-600">
               <p>
